7bd494964f47e20dd5501fb9337078199dc69dc0
Aaron <aaronkanaron@gmail.com>
2021-10-03 21:14:35 +0200

    Init

---
+++ index.js
@@ -0,0 +1,38 @@
+const api_key = "3555d01f29466ed45ea26ca820ff7474"
+let page = 1
+
+
+
+async function loadPopular(currentPage){
+	await fetch("https://api.themoviedb.org/3/movie/popular?api_key="+ api_key + "&language=en-US&page=" + currentPage, { 
+		method: "GET"
+			
+		}).then((response) => {
+			return response.json();
+
+		}).then((data)=> {
+			data.results.map((obj) => {
+				document.getElementById("popular-scrolling-wrapper").innerHTML += 
+				`
+				<div class="card" onclick="window.location.href = 'movies.html?id=` + obj.id +`'">
+					<img src="https://image.tmdb.org/t/p/w500` + obj.poster_path + `" alt="` + obj.title + `">
+					<!--
+					<div class="overlay"></div>
+					<div class="wrapper">
+						<h1>` + obj.title + `</h1>
+						<p>` + obj.overview +`</p>
+					</div>
+					-->
+				</div>
+				`;
+					
+			})
+		})
+}
+
+function nextPage() {
+	page += 1
+	loadPopular(page)
+}
+
+loadPopular()
---
+++ movies.js
@@ -0,0 +1,71 @@
+
+window.addEventListener("DOMContentLoaded", () => {
+	const urlParams = new URLSearchParams(window.location.search);
+	movie = urlParams.get('id')
+	loadMovie(movie);
+})
+
+
+async function loadMovie(movie_id) {
+	Promise.all([
+			fetch("https://api.themoviedb.org/3/movie/" + movie_id + "?api_key=" + env.api_key + "&language=en-US"), 
+			fetch("https://api.themoviedb.org/3/movie/" + movie_id + "/watch/providers?api_key=" + env.api_key)
+		]).then((responses) => {
+			return Promise.all(responses.map((response) => {
+				return response.json();
+			}));
+
+		}).then((data) => {
+			const background = document.getElementById("background")
+			/*
+
+			Det kan vara både Buy - Rent och Flatrate, se till att alla kan bli valda och bygg en meny med dess index.
+
+			*/
+
+			flatrate = ""
+			rent = ""
+			buy = ""
+			// data[1].results.US.hasOwnProperty("flatrate") ? data[1].results.US.flatrate.map((obj, index) => { Object.keys(data[1].results.US.flatrate).length-1 > index ? flatrate += obj.provider_name + ", " : flatrate += obj.provider_name }) : flatrate = "You cannot stream this movie"
+
+			// data[1].results.US.hasOwnProperty("rent") ? data[1].results.US.rent.map((obj, index) => { Object.keys(data[1].results.US.rent).length-1 > index ? rent += obj.provider_name + ", " : rent += obj.provider_name }) : rent = "You cannot rent this movie"
+
+			// data[1].results.US.hasOwnProperty("buy") ? data[1].results.US.buy.map((obj, index) => { Object.keys(data[1].results.US.buy).length-1 > index ? buy += obj.provider_name + ", " : buy += obj.provider_name }) : buy = "You cannot buy this movie"
+
+			data[1].hasOwnProperty("results") ? data[1].results.hasOwnProperty("US") ? data[1].results.US.hasOwnProperty("flatrate") ? data[1].results.US.flatrate.map((obj) => { flatrate += "<img src='https://image.tmdb.org/t/p/original" + obj.logo_path + "'>" }) : flatrate = "<p>You cannot stream this movie</p>" : flatrate = "<p>This movie has not come out</p>" : window.location.href = "404.html"
+
+			data[1].results.hasOwnProperty("US") ? data[1].results.US.hasOwnProperty("rent") ? data[1].results.US.rent.map((obj) => { rent += "<img src='https://image.tmdb.org/t/p/original" + obj.logo_path + "'>" }) : rent = "<p>You cannot rent this movie</p>" : rent = "<p>This movie has not come out</p>"
+
+			data[1].results.hasOwnProperty("US") ? data[1].results.US.hasOwnProperty("buy") ? data[1].results.US.buy.map((obj) => { buy += "<img src='https://image.tmdb.org/t/p/original" + obj.logo_path + "'>" }) : buy = "<p>You cannot buy this movie</p>" : buy = "<p>This movie has not come out</p>"
+
+			background.style.backgroundImage = "url(https://image.tmdb.org/t/p/original" + data[0].backdrop_path +")"
+			
+			//Get Genres
+			// let genres = "";
+			// data.genres.map((obj) => { genres += obj.name + ", "; return genres })
+			// let companies = "";
+			// data.production_companies.map((obj) => { companies += obj.name + ", "; return companies })
+
+			//Append content
+			background.innerHTML += 
+			`
+				<div class="wrapper">
+					<h1>` + data[0].title +`</h1>
+
+					<p>` + data[0].overview +`</p>
+				</div>
+
+				<div class="background_title">
+						<span>` + data[0].title +`</span>
+				</div>
+			`
+			document.getElementById("content").innerHTML += data[1].results.hasOwnProperty("US") ? data[1].results.US.hasOwnProperty("flatrate") ? flatrate : data[1].results.US.hasOwnProperty("rent") ? rent : data[1].results.US.hasOwnProperty("buy") ? buy : "<p>This movie is not available.</p>" : "<p>This movie is not available.</p>"
+		})
+}
+
+
+function changeActiveButton(){
+	document.getElementById("category").childNodes.forEach((obj) => {
+		obj.classList.remove("active")
+	})
+}
